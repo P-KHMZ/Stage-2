fe3dd0fa351e4a67f026d012afcef8b6b80d6a5a
Peter <pierrekahumuza@outlook.com>
2021-08-20 13:45:55 -0700

    last commit

--- config/config.jp.php
+++ config/config.jp.php
@@ -11,12 +11,12 @@
     $db = substr($cleardb_url["path"], 1);
     // $db = $cleardb_url["heroku_3a028db4874a8f2"]
 
-    $active_group = 'default';
-    $query_builder = TRUE
+    // $active_group = 'default';
+    // $query_builder = TRUE
 
     $con = mysqli_connect($server,$username,$password, $db) or die("Couln't connect to the database: ".mysqli_connect_error());
 
-    // $con = mysqli_connect("localhost", "root", "", "zuridb") or die("Couln't connect to the database: ".mysqli_connect_error());
+    $con = mysqli_connect("localhost", "root", "", "zuridb") or die("Couln't connect to the database: ".mysqli_connect_error());
 
     $errors = [];
 
